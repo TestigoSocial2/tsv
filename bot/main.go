7f2b36fdebd39e158ce05f7e57eb2b5d69257cdc
Ben Cessa <ben@pixative.com>
2017-05-26 23:48:32 -0500

    New bot structure

--- bot/main.go
+++ bot/main.go
@@ -24,18 +24,18 @@ import (
   "bytes"
   "fmt"
   "io/ioutil"
-  "log"
   "net/http"
   
-  "github.com/Jeffail/gabs"
   "github.com/julienschmidt/httprouter"
+  "encoding/json"
 )
 
-// Bot provides the functionality to implement a Facebook Messenger agent
-type Bot struct {
+// Instance provides the functionality to implement a Facebook Messenger agent
+type Instance struct {
   verificationToken string
   pageToken         string
-  IncomingMessages  chan *IncomingMessage
+  IncomingMessages  chan *Callback
+  ReceptionErrors   chan error
 }
 
 // Config defines the required parameters to start the bot
@@ -44,23 +44,37 @@ type Config struct {
   PageToken         string
 }
 
-// IncomingMessage are messages received from Facebook users
-type IncomingMessage struct {
-  User    string
-  Content string
-}
-
 // New will create and return a bot instance
-func New(c *Config) *Bot {
-  return &Bot{
+func New(c *Config) *Instance {
+  return &Instance{
     verificationToken: c.VerificationToken,
     pageToken:         c.PageToken,
-    IncomingMessages:  make(chan *IncomingMessage, 10),
+    IncomingMessages:  make(chan *Callback, 10),
+    ReceptionErrors:   make(chan error),
   }
 }
 
-// Verify handle the endpoint verification process
-func (b *Bot) Verify(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// Utility method to properly dispatch a request to FB's API
+func (b *Instance) dispatch(content []byte) ([]byte, error) {
+  // Build request
+  url := fmt.Sprintf("%s=%s", "https://graph.facebook.com/v2.6/me/messages?access_token", b.pageToken)
+  req, _ := http.NewRequest("POST", url, bytes.NewBuffer(content))
+  req.Header.Set("Content-Type", "application/json")
+  
+  // Send request
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    return nil, err
+  }
+  defer resp.Body.Close()
+  
+  // Return body
+  return ioutil.ReadAll(resp.Body)
+}
+
+// Verify handle the endpoint for the verification process
+func (b *Instance) Verify(w http.ResponseWriter, r *http.Request) {
   w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
   mode := r.FormValue("hub.mode")
   token := r.FormValue("hub.verify_token")
@@ -72,48 +86,24 @@ func (b *Bot) Verify(w http.ResponseWriter, r *http.Request, _ httprouter.Params
   fmt.Fprintf(w, fmt.Sprintf("%s", "INVALID_VERIFICATION_TOKEN"))
 }
 
-// ReceiveMessage ...
-func (b *Bot) ReceiveMessage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// ReceiveMessage handle the message delivery endpoint
+func (b *Instance) ReceiveMessage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   // Get request body and return response
   body, err := ioutil.ReadAll(r.Body)
   if err != nil {
-    log.Printf("error: %s", err)
+    b.ReceptionErrors <- err
   }
-  fmt.Fprintf(w, "%s", "")
   
-  // Parse and process message contents
-  data, _ := gabs.ParseJSON(body)
-  if data.Path("object").Data().(string) == "page" {
-    entries, _ := data.Search("entry").Children()
-    for _, entry := range entries {
-      messages, _ := entry.Search("messaging").Children()
-      for _, m := range messages {
-        b.IncomingMessages <- &IncomingMessage{
-          User:    m.Path("sender.id").Data().(string),
-          Content: m.Path("message.text").Data().(string),
-        }
-      }
-    }
+  var cb Callback
+  err = json.Unmarshal(body, &cb)
+  if err != nil {
+    b.ReceptionErrors <- err
   }
+  
+  b.IncomingMessages <- &cb
 }
 
 // DispatchMessage use Facebook's Messenger to reach users
-func (b *Bot) DispatchMessage(recipient string, content string) error {
-  // Build response
-  msgResponse := gabs.New()
-  msgResponse.Set(recipient, "recipient", "id")
-  msgResponse.Set(content, "message", "text")
-  
-  // Dispatch response
-  url := fmt.Sprintf("%s=%s", "https://graph.facebook.com/v2.6/me/messages?access_token", b.pageToken)
-  req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(msgResponse.String())))
-  req.Header.Set("Content-Type", "application/json")
-  client := &http.Client{}
-  resp, err := client.Do(req)
-  if err != nil {
-    return err
-  }
-  defer resp.Body.Close()
-  
-  return nil
+func (b *Instance) DispatchMessage(msg string) ([]byte, error) {
+  return b.dispatch([]byte(msg))
 }
--- bot/message.go
+++ bot/message.go
@@ -20,96 +20,127 @@
 
 package bot
 
-// Basic structures
-type sender struct {
-  ID string `json:"id"`
+type recipient struct {
+  ID          string `json:"id,omitempty"`
+  PhoneNumber string `json:"phone_number,omitempty"`
+  Name struct {
+    FirstName string `json:"first_name,omitempty"`
+    LastName  string `json:"last_name,omitempty"`
+  } `json:"name,omitempty"`
 }
 
-type recipient struct {
-  ID string `json:"id"`
+type sender struct {
+  ID string `json:"id,omitempty"`
 }
 
 type quickReply struct {
-  Payload string `json:"payload"`
+  Payload string `json:"payload,omitempty"`
 }
 
 type location struct {
   Coordinates struct {
-    Lat  float64 `json:"lat"`
-    Long float64 `json:"long"`
-  } `json:"coordinates"`
+    Lat  float64 `json:"lat,omitempty"`
+    Long float64 `json:"long,omitempty"`
+  } `json:"coordinates,omitempty"`
 }
 
-type attachmentPayload struct {
-  URL      string   `json:"url"`
-  Location location `json:"location"`
+type urlButton struct {
+  Type                string `json:"type,omitempty"`
+  Title               string `json:"title,omitempty"`
+  URL                 string `json:"url,omitempty"`
+  MessengerExtensions bool   `json:"messenger_extensions,omitempty"`
+  FallbackURL         string `json:"fallback_url,omitempty"`
+  WebviewHeightRatio  string `json:"webview_height_ratio,omitempty"`
+  WebviewShareButton  string `json:"webview_share_button,omitempty"`
+}
+
+type element struct {
+  Title         string      `json:"title,omitempty"`
+  Subtitle      string      `json:"subtitle,omitempty"`
+  DefaultAction urlButton   `json:"default_action,omitempty"`
+  Buttons       []urlButton `json:"buttons,omitempty"`
+}
+
+type payload struct {
+  URL              string      `json:"url,omitempty"`
+  Type             string      `json:"type,omitempty"`
+  Text             string      `json:"text,omitempty"`
+  Buttons          []urlButton `json:"buttons,omitempty"`
+  Title            string      `json:"title,omitempty"`
+  Location         location    `json:"location,omitempty"`
+  TemplateType     string      `json:"template_type,omitempty"`
+  Sharable         bool        `json:"sharable,omitempty"`
+  ImageAspectRatio string      `json:"image_aspect_ratio,omitempty"`
+  Elements         []element   `json:"elements,omitempty"`
 }
 
 type attachment struct {
-  Type    string            `json:"type"`
-  Payload attachmentPayload `json:"payload"`
+  Type    string  `json:"type,omitempty"`
+  Title   string  `json:"title,omitempty"`
+  URL     string  `json:"url,omitempty"`
+  Payload payload `json:"payload,omitempty"`
 }
 
-type delivery struct {
-  Mids      []string `json:"mids"`
-  Watermark int64    `json:"watermark"`
-  Seq       int64    `json:"seq"`
-}
-
-// MessageReceived represents an incoming message notification
-// Endpoint: message
-type MessageReceived struct {
-  Sender    sender    `json:"sender"`
-  Recipient recipient `json:"recipient"`
-  Timestamp int64     `json:"timestamp"`
+type messaging struct {
+  Recipient recipient `json:"recipient,omitempty"`
+  Sender    sender    `json:"sender,omitempty"`
+  Timestamp int64     `json:"timestamp,omitempty"`
   Message struct {
-    Mid         string       `json:"mid"`
-    Seq         int64        `json:"seq"`
-    Text        string       `json:"text"`
-    QuickReply  quickReply   `json:"quick_reply"`
-    Attachments []attachment `json:"attachments"`
-  } `json:"message"`
-}
-
-// MessageDelivered represents an outgoing message notification
-// Endpoint: message_deliveries
-type MessageDelivered struct {
-  Sender    sender     `json:"sender"`
-  Recipient recipient  `json:"recipient"`
-  Delivery  []delivery `json:"delivery"`
-}
-
-// MessageRead represents a 'messages read' notification
-// Endpoint: message_reads
-type MessageRead struct {
-  Sender    sender    `json:"sender"`
-  Recipient recipient `json:"recipient"`
-  Timestamp int64     `json:"timestamp"`
+    Mid         string       `json:"mid,omitempty"`
+    Seq         int64        `json:"seq,omitempty"`
+    Text        string       `json:"text,omitempty"`
+    QuickReply  quickReply   `json:"quick_reply,omitempty"`
+    Attachments []attachment `json:"attachments,omitempty"`
+    IsEcho      bool         `json:"is_echo,omitempty"`
+    AppID       string       `json:"app_id,omitempty"`
+    Metadata    string       `json:"metadata,omitempty"`
+  } `json:"message,omitempty"`
+  Delivery struct {
+    Mids      []string `json:"mids,omitempty"`
+    Watermark int64    `json:"watermark,omitempty"`
+    Seq       int64    `json:"seq,omitempty"`
+  } `json:"delivery,omitempty"`
   Read struct {
-    Watermark int64 `json:"watermark"`
-    Seq       int64 `json:"seq"`
-  } `json:"read"`
-}
-
-// MessagePostBack represents a user action
-// Endpoint: messaging_postbacks
-type MessagePostBack struct {
-  Sender    sender    `json:"sender"`
-  Recipient recipient `json:"recipient"`
-  Timestamp int64     `json:"timestamp"`
+    Watermark int64 `json:"watermark,omitempty"`
+    Seq       int64 `json:"seq,omitempty"`
+  } `json:"read,omitempty"`
   Postback struct {
-    Payload string `json:"payload"`
-  } `json:"postback"`
+    Payload string `json:"payload,omitempty"`
+    Referral struct {
+      Ref    string `json:"ref,omitempty"`
+      AdID   string `json:"ad_id,omitempty"`
+      Source string `json:"source,omitempty"`
+      Type   string `json:"type,omitempty"`
+    } `json:"referral,omitempty"`
+  } `json:"postback,omitempty"`
+  Optin struct {
+    Ref string `json:"ref,omitempty"`
+  } `json:"optin,omitempty"`
+  AccountLinking struct {
+    Status            string `json:"status,omitempty"`
+    AuthorizationCode string `json:"authorization_code,omitempty"`
+  } `json:"account_linking,omitempty"`
 }
 
-// AccountLinking represents an authorization action
-// Endpoint: messaging_postbacks
-type AccountLinking struct {
-  Sender    sender    `json:"sender"`
-  Recipient recipient `json:"recipient"`
-  Timestamp int64     `json:"timestamp"`
-  Link struct {
-    Status            string `json:"status"`
-    AuthorizationCode string `json:"authorization_code"`
-  } `json:"account_linking"`
+type entry struct {
+  ID        string      `json:"id,omitempty"`
+  Time      int64       `json:"time,omitempty"`
+  Messaging []messaging `json:"messaging,omitempty"`
+}
+
+type Callback struct {
+  Object string  `json:"object,omitempty"`
+  Entry  []entry `json:"entry,omitempty"`
+}
+
+type Msg struct {
+  Recipient        recipient `json:"recipient,omitempty"`
+  SenderAction     string    `json:"sender_action,omitempty"`
+  NotificationType string    `json:"notification_type,omitempty"`
+  Message struct {
+    Text         string       `json:"text,omitempty"`
+    Metadata     string       `json:"metadata,omitempty"`
+    QuickReplies []quickReply `json:"quick_replies,omitempty"`
+    Attachment   attachment   `json:"attachment,omitempty"`
+  } `json:"message,omitempty"`
 }
