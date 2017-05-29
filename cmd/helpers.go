0881d169cd21b1bd1343b1a5820984a57d375b54
Ben Cessa <ben@pixative.com>
2017-05-29 01:52:09 -0500

    Add helper methods for the flow bot messages

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -24,6 +24,10 @@ import (
   "os"
   "log"
   "github.com/transparenciamx/tsv/storage"
+  "github.com/transparenciamx/tsv/bot"
+  "encoding/json"
+  "github.com/Jeffail/gabs"
+  "fmt"
 )
 
 // Utility method to open a storage handler
@@ -41,4 +45,86 @@ func connectStorage(host, database string) (*storage.Handler, error ) {
   }
   log.Printf("Using storage: %s/%s", host, database)
   return db, nil
+}
+
+// GetWelcomeMessage returns a ready to use 'welcome message' for a new user
+func GetWelcomeMessage(user string) *bot.Message {
+  return &bot.Message{
+    Recipient: bot.Recipient{
+      ID: user,
+    },
+    Message: bot.MessageBody{
+      Attachment: bot.Attachment{
+        Type: "template",
+        Payload: bot.Payload{
+          TemplateType: "button",
+          Text: "Elije una de las siguientes opciones:",
+          Buttons: []bot.URLButton{
+            {
+              Type: "postback",
+              Title: "Nueva Consulta",
+              Payload: "NEW_QUERY",
+            },
+            {
+              Type: "web_url",
+              URL: "https://www.testigosocial.mx",
+              Title: "Visitar Sitio Web",
+            },
+          },
+        },
+      },
+    },
+  }
+}
+
+// GetQueryMenu returns a menu with the available basic queries
+func GetQueryMenu(user string) *bot.Message {
+  return &bot.Message{
+    Recipient: bot.Recipient{
+      ID: user,
+    },
+    Message: bot.MessageBody{
+      Text: "Seleccione el criterio para su consulta de los procesos de contratación registrados:",
+      QuickReplies: []bot.QuickReply{
+        {ContentType: "text", Title: "Recientes", Payload: "RECENT"},
+        {ContentType: "text", Title: "Mayor Monto", Payload: "AMOUNT"},
+        {ContentType: "text", Title: "Grupo Aeroportuario", Payload: "GACM"},
+        {ContentType: "text", Title: "Ciudad de México", Payload: "CDMX"},
+      },
+    },
+  }
+}
+
+// GetContractListMessage return a list message based on the provided query results
+func GetContractListMessage(user string, list []interface{}) *bot.Message {
+  els := []bot.ListElement{}
+  for _, rec := range list {
+    json, _ := json.Marshal(rec)
+    r, _ := gabs.ParseJSON(json)
+    releases, _ := r.Search("releases").Children()
+    els = append(els, bot.ListElement{
+      Title: releases[0].Path("ocid").String(),
+      Subtitle: releases[0].Path("tender.title").String(),
+      DefaultAction: bot.URLButton{
+        Type: "web_url",
+        URL: fmt.Sprintf("https://www.testigosocial.mx/contratos/%s", r.Path("_id").Data().(string)),
+      },
+    })
+  }
+  
+  return &bot.Message{
+    Recipient: bot.Recipient{
+      ID: user,
+    },
+    Message: bot.MessageBody{
+      Attachment: bot.Attachment{
+        Type: "template",
+        Payload: bot.Payload{
+          TemplateType: "list",
+          TopElementStyle: "compact",
+          Elements: els,
+        },
+      },
+    },
+  }
 }
